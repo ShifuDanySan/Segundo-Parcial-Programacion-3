c7cc8d237e16cf93466c5d9d248bb1f4fbf2749a
Dany San <7dany777@gmail.com>
2020-06-29 17:26:38 -0300

    metodos que obtienen la informacion del archivo de entrada

--- Segundo Parcial Programacion 3/src/paquete/Archivo.java
+++ Segundo Parcial Programacion 3/src/paquete/Archivo.java
@@ -18,33 +18,36 @@ public class Archivo {
 			texto = JOptionPane.showInputDialog("Ingrese el texto\n");
 			texto += "\r\n";
 			archivo.write(texto);
-			archivo.close();// cierro el archivo
-		} catch (IOException e) {// controlo excepciones
+			archivo.close();
+		} catch (IOException e) {
 			System.out.println("No se encuentra el archivo");
 		}
 	}
 
 	public String leeArchivo() {
+		this.cantidadLineasArchivo = 0;
 		String texto = "";
 		try {
 			BufferedReader bf = new BufferedReader(new FileReader(this.direccion));
 			String temp = "";
 			String bfRead;
-			while ((bfRead = bf.readLine()) != null) {// Este while avanza linea por linea
-				temp += bfRead;// guardo el contenido de la linea del archivo en la variable temporal
-				temp += "\n";/*
-								 * guardo en la variable String temporal un salto de linea antes de guardar la
-								 * siguente linea en esta variable
-								 */
+			while ((bfRead = bf.readLine()) != null) {
+				this.cantidadLineasArchivo++;
+				temp += bfRead;
+				temp += "\n";
+
 			}
-			texto = temp;// aqui asigno el contenido de temp a la variable String que retorno
-			bf.close();// cierro el archivo
-		} catch (IOException e) {// controlo excepciones
+			bf.close();
+		} catch (IOException e) {
 			System.out.println("No se encuentra el archivo");
 		}
 		return texto;
 	}
-	
-	
-	String direccion;
+
+	public int cantidadLineasArchivo() {
+		return this.cantidadLineasArchivo;
+	}
+
+	private String direccion = "";
+	private int cantidadLineasArchivo = 0;
 }
--- Segundo Parcial Programacion 3/src/paquete/estadoComputadoras.java
+++ Segundo Parcial Programacion 3/src/paquete/estadoComputadoras.java
@@ -1,17 +1,52 @@
 package paquete;
 
 public class estadoComputadoras {
-	public estadoComputadoras(char avisoOConsulta, int k1, int k2) {// aviso de si es pregunta o aviso de que esta conectada
+	public estadoComputadoras() {// aviso de si es pregunta o aviso de que esta
+																	// conectada
 		this.avisoOConsulta = avisoOConsulta;
 		this.k1 = k1;
 		this.k2 = k2;
-		if (this.avisoOConsulta == 'c') {
-			this.k1Conectada = true;
-			this.k1Conectada = true;
+
+	}
+
+	public void cargarDatosComputadora(String lin) {
+		linea = lin;
+		aux = "";
+		if (linea.equalsIgnoreCase("F") == false) {
+			for (int i = 0; i < this.linea.length(); i++) {
+				if (linea.charAt(i) != ' ') {
+					aux += linea.charAt(i);
+				} else {
+					contadorEspacios++;
+
+					if (contadorEspacios == 1) {
+						this.avisoOConsulta = linea.charAt(0);
+						aux = "";
+					}
+
+					if (contadorEspacios == 2) {
+						this.k1 = Integer.parseInt(aux);
+						aux = "";
+					}
+				}
+				if (i == linea.length() - 1) {
+					this.k2 = Integer.parseInt(aux);
+					aux = "";
+				}
+			}
+			this.linea = "";
+			contadorEspacios = 0;
+			this.linea = "";
+			aux = "";
+
+			if (this.avisoOConsulta == 'c') {
+				this.k1Conectada = true;
+				this.k2Conectada = true;
+			}
 		}
 	}
 
-	public int devuelveavisoOConsulta() {
+	public char devuelveavisoOConsulta() {
 		return this.avisoOConsulta;
 	}
 
@@ -28,4 +63,7 @@ public class estadoComputadoras {
 	private int k2 = 0;
 	private boolean k1Conectada = false;
 	private boolean k2Conectada = false;
+	private String linea = "";
+	private String aux = "";
+	private int contadorEspacios = 0;
 }
