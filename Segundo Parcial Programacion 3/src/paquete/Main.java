9e87546a599600521e3790df8ed1ddd1c48e2ac3
Dany San <7dany777@gmail.com>
2020-06-29 14:06:19 -0300

    Pueba de actualizacion de github 3.1

--- Segundo Parcial Programacion 3/src/paquete/Main.java
+++ Segundo Parcial Programacion 3/src/paquete/Main.java
@@ -3,11 +3,7 @@ package paquete;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Apéndice de método generado automáticamente
-		
-		///EN ESPERA DEL PARCIAL///
-		///SEGUIMOS EN ESPERA DEL PARCIAL///
-
+		//hoy es el parcial
 	}
 
 }
